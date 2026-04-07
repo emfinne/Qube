085b9429b0aacccc61ba5e403ce2047ebb5a77bd
parallels <emfinne13@gmail.com>
2026-04-07 19:31:34 +0200

    Remove build, install, log directories

--- build/qube_bringup/CMakeFiles/qube_bringup_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qube_bringup_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qube_bringup_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
